bbd84f815a993196f0e689c13f38fcddad36cd31
prabutdr <prabutdr@gmail.com>
2020-04-16 21:45:45 +0530

    naive approach

--- src/practice/problem-patterns/freq-counter-same.ts
+++ src/practice/problem-patterns/freq-counter-same.ts
@@ -2,9 +2,6 @@
  * Should return true if every value in the array has it's
  * corresponding value squared in the second array.
  * The frequency of values must be the same
- *
- * @param nums
- * @param squaredNums
  */
 export function same(nums: number[], squaredNums: number[]): boolean {
   if (nums.length !== squaredNums.length) return false;
@@ -12,15 +9,34 @@ export function same(nums: number[], squaredNums: number[]): boolean {
   const map = new Map<number, number>();
   for (const num of squaredNums) {
     const freq = map.get(num);
+
     map.set(num, freq === undefined ? 1 : freq + 1);
   }
 
   for (const num of nums) {
     const sn = num ** 2;
     const freq = map.get(sn);
+
     if (freq === undefined || freq <= 0) return false;
     else map.set(sn, freq - 1);
   }
 
   return [...map.values()].every((freq) => freq == 0);
+}
+
+export function sameNaiveApproach(
+  nums: number[],
+  squaredNums: number[],
+): boolean {
+  if (nums.length !== squaredNums.length) return false;
+
+  for (const num of nums) {
+    const index = squaredNums.indexOf(num ** 2);
+
+    if (index === -1) return false;
+
+    squaredNums.splice(index, 1);
+  }
+
+  return true;
 }
--- test/practice/problem-patterns/freq-counter-same.test.ts
+++ test/practice/problem-patterns/freq-counter-same.test.ts
@@ -1,5 +1,8 @@
 import each from 'jest-each';
-import { same } from '../../../src/practice/problem-patterns/freq-counter-same';
+import {
+  same,
+  sameNaiveApproach,
+} from '../../../src/practice/problem-patterns/freq-counter-same';
 
 describe('same', () => {
   each([
@@ -21,6 +24,10 @@ describe('same', () => {
       [1, 2, 3],
       [1, 9],
     ],
+    [
+      [1, 2, 3],
+      [1, 4, 9, 10],
+    ],
     [[1, 2, 3], [1]],
     [
       [1, 2, 1],
@@ -30,6 +37,43 @@ describe('same', () => {
     'should not be same for %j and %j',
     (nums: number[], squaredNums: number[]) => {
       expect(same(nums, squaredNums)).toBe(false);
+    },
+  );
+});
+
+describe('sameNaiveApproach', () => {
+  each([
+    [
+      [1, 2, 3],
+      [4, 1, 9],
+    ],
+    [[1], [1]],
+    [[], []],
+  ]).test(
+    'should be same for %j and %j',
+    (nums: number[], squaredNums: number[]) => {
+      expect(sameNaiveApproach(nums, squaredNums)).toBe(true);
+    },
+  );
+
+  each([
+    [
+      [1, 2, 3],
+      [1, 9],
+    ],
+    [
+      [1, 2, 3],
+      [1, 4, 9, 10],
+    ],
+    [[1, 2, 3], [1]],
+    [
+      [1, 2, 1],
+      [4, 4, 1],
+    ],
+  ]).test(
+    'should not be same for %j and %j',
+    (nums: number[], squaredNums: number[]) => {
+      expect(sameNaiveApproach(nums, squaredNums)).toBe(false);
     },
   );
 });
